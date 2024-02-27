ecaebd0ccaff6a8240f3ddbd64b41ade65100f0c
Bun <2910752440@qq.com>
2024-02-27 15:52:15 +0800

    283. 移动零

--- LeetCode.java
+++ LeetCode.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -5,57 +6,23 @@ import java.util.Map;
 
 public class LeetCode {
     public static void main(String[] args) {
-        int[] m_inorder={9,3,15,20,7};
-        int[] m_postorder={9,15,7,20,3};
-        Solution s=new Solution();
-        System.out.println(s.buildTree(m_inorder, m_postorder));
+
 
     }
 }
 class Solution {
-
-    int[] m_inorder;
-    int[] m_postorder;
-    int rootIndex;
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        this.m_inorder=inorder;
-        this.m_postorder=postorder;
-        rootIndex=postorder.length-1;
-        return help(0, inorder.length - 1);
-    }
-    public TreeNode help(int left, int right)
-    {
-        if (left>right)
-        {
-            return null;
+    public void moveZeroes(int[] nums) {
+        int j=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]!=0)
+            {
+                nums[j]=nums[i];
+                j++;
+            }
         }
-        TreeNode root=new TreeNode(m_postorder[rootIndex]);
-        int index = findIndex(rootIndex);
-        rootIndex--;
-        root.right=help(index+1,right);
-        root.left=help(left,index-1);
-        return root;
-    }
-
-    public int findIndex(int rootIndex)
-    {
-        for (int i = 0; i < m_inorder.length; i++) {
-            if (m_inorder[i]==m_postorder[rootIndex])return i;
+        for (; j < nums.length; j++) {
+            nums[j]=0;
         }
-        return -1;
-    }
-
-
-}
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
     }
 }
+
