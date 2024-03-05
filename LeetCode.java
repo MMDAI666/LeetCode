f955bd27f550bb9cc953e4a202c0024fbde309dc
Bun <2910752440@qq.com>
2024-03-05 21:23:12 +0800

    3. 无重复字符的最长子串

--- LeetCode.java
+++ LeetCode.java
@@ -10,65 +10,26 @@ public class LeetCode {
 
 
 /*
-42. 接雨水
-给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+*
+3. 无重复字符的最长子串
+给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串的长度。
 */
 class Solution {
-    public int trap(int[] height) {
-        int res=0;
-        int n = height.length;
-        if (n<=1)return 0;
-
-        int[] lMax = new int[n];//保存从左向右i的最大高度
-        lMax[0]=height[0];
-
-        int[] rMax = new int[n];//保存从右向左i的最大高度
-        rMax[n-1]=height[n-1];
-        int j=n-2;
-
-        //遍历得到lMax和rMax
-        for (int i = 1; i < n; i++) {
-            lMax[i]=Math.max(height[i],lMax[i-1]);
-
-            rMax[j]=Math.max(height[j],rMax[j+1]);
-            j--;
-        }
-
-        //计算每个i位置能接的水量
-        for (int i = 0; i < n; i++) {
-            if (rMax[i]<=height[i] || lMax[i]<=height[i])continue;
-            res +=Math.min(rMax[i],lMax[i])-height[i];
-        }
-        return res;
-    }
-
-
-    //双指针
-    public int trap(int[] height){
-        int res=0;
-        int left=0;
-        int n=height.length;
-        int right=n-1;
-
-        int lMax=height[left];
-        int rMax=height[right];
-
-        while (left<right)
+    public int lengthOfLongestSubstring(String s) {
+        if(s.isEmpty())return 0;
+        if(s.length()==1)return 1;
+        int max=1,left=0;
+        Map<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++)
         {
-            if (lMax>rMax)
+            if(map.containsKey(s.charAt(i)))
             {
-                if (rMax>height[right]) res+=rMax-height[right];
-                right--;
-                rMax=Math.max(height[right],rMax);
+                left=Math.max(map.get(s.charAt(i))+1,left);//判断重复字符是否在窗口内
             }
-            else {
-                if (lMax>height[left]) res+=lMax-height[left];
-                left++;
-                lMax=Math.max(height[left],lMax);
-            }
+            map.put(s.charAt(i),i);
+            max=Math.max(max,i-left+1);
         }
-        return res;
-
+        return max;
     }
 }
 
