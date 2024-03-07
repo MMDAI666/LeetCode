1d252e9f76ca22b67eec958abf502697a35953b7
Bun <2910752440@qq.com>
2024-03-07 09:37:16 +0800

    239. 滑动窗口最大值

--- LeetCode.java
+++ LeetCode.java
@@ -13,59 +13,95 @@ public class LeetCode {
 
 /*
 *
-143. 重排链表
-* 给定一个单链表 L 的头节点 head ，单链表 L 表示为：
-
-L0 → L1 → … → Ln - 1 → Ln
-请将其重新排列后变为：
-
-L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
-不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。
+* 你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+返回 滑动窗口中的最大值 。
 */
-//Definition for singly-linked list.
- class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
 
 class Solution {
-    public void reorderList(ListNode head) {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length==1)return new int[]{nums[0]};
 
-        List<ListNode> list=new ArrayList<>();
-        ListNode ans=head;
-        while (head!=null)
-        {
-            list.add(head);
-            head=head.next;
+        int[] ans=new int[nums.length-k+1];
+        int max=nums[0];
+        //计算出第一个窗口的max
+        for (int i = 0; i < k; i++) {
+            if (nums[i]>max) {
+                max = nums[i];
+            }
         }
+        ans[0]=max;
+        if (k==nums.length)return ans;
 
+        //计算窗口内最大的数的个数
+        int maxNum=0;
+        for (int i = 0; i < k; i++) {
+            if (nums[i]==max)maxNum++;
+        }
+        int count=1;
+        int r=k;//r为即将进入窗口的元素，l为即将离开窗口的元素
 
-        int len=list.size();
-        int j=len-1;
-        boolean flag=true;
-        for (int i = 0; i <j; ) {
-            //ans=ans.next;
-            //ans=list.get(i);
-            if (ans==null)return;
-            if (flag)
+        for (int l = 0; l < nums.length-k; l++) {
+            //右边加一个
+            if (nums[r]>=max) {
+                if (nums[r]==max)
+                {
+                    maxNum++;
+                }else maxNum=1;
+                max = nums[r];
+            }
+
+            //左边减一个
+            if (nums[l]>=max)
             {
-                ans.next=list.get(j);
-                j--;
-                flag=false;
+                maxNum--;
+                if (maxNum==0)
+                {//去掉了唯一最大值，更新最大值
+                    max=nums[l+1];
+                    for (int j=l+1;j<=r;j++)
+                    {
+                        if (nums[j]>max)max=nums[j];
+                    }
+                    for (int j=l+1;j<=r;j++)
+                    {
+                        if (nums[j]==max)maxNum++;
+                    }
+                }
             }
-            else {
-                ans.next=list.get(i);
-                i++;
-                flag=true;
-            }
-            ans=ans.next;
+            r++;
+            ans[count++]=max;
+
         }
-        ans.next=null;
+        return ans;
+    }
+
+    //单调队列
+    public int[] maxSlidingWindowByQueue(int[] nums, int k) {
+        if (nums.length==1)return new int[]{nums[0]};
+        int n=nums.length;
+        int[] ans=new int[nums.length-k+1];
+        Deque<Integer> deque=new ArrayDeque<>();
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty()&&nums[i]>=deque.peekLast())
+            {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+        }
+        ans[0]=nums[deque.peekFirst()];
+
+        for (int i = k; i < n; ++i) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            while (deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            ans[i - k + 1] = nums[deque.peekFirst()];
+        }
+        return ans;
 
 
     }
-
 }
