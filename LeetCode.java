37091d4dcdbd116f73c0dbb2fd25569a16dafe4c
Bun <2910752440@qq.com>
2024-03-08 17:07:36 +0800

    21. 合并两个有序链表

--- LeetCode.java
+++ LeetCode.java
@@ -21,25 +21,33 @@ class ListNode {
 }
 
 class Solution {
-    public ListNode detectCycle(ListNode head) {
-
-        ListNode fast=head;
-        ListNode slow=head;
-        while (fast!=null&&fast.next!=null)
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1==null ||list2==null)return list1==null?list2:list1;
+        ListNode head;
+        if (list1.val<=list2.val)
         {
-            fast=fast.next.next;
-            slow=slow.next;
-            if (slow==fast)
-            {
-                while (head!=slow)
-                {
-                    head=head.next;
-                    slow=slow.next
-                }
-                return head;
-            }
+            head=list1;
+            list1= list1.next;
+        }else
+        {
+            head=list2;
+            list2=list2.next;
         }
-
-        return null;
+        ListNode res=head;
+        while (list1!=null &&list2!=null)
+        {
+            if (list1.val<=list2.val)
+            {
+                head.next=list1;
+                list1= list1.next;
+            }else
+            {
+                head.next=list2;
+                list2=list2.next;
+            }
+            head=head.next;
+        }
+        head.next=list1==null?list2:list1;
+        return res;
     }
 }
