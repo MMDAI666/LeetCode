f72a7ef2567f576d06f8b526ccff0b55560fc44c
Bun <2910752440@qq.com>
2024-03-08 14:41:17 +0800

    240. 搜索二维矩阵 II

--- LeetCode.java
+++ LeetCode.java
@@ -4,41 +4,26 @@ import java.util.stream.Collectors;
 
 public class LeetCode {
     public static void main(String[] args) {
-        int [][] ma={
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
-        };
-        new Solution().rotate(ma);
-        System.out.println(Arrays.deepToString(ma));
+
 
     }
 }
 
 
 class Solution {
-
-    public void rotate(int[][] matrix) {
-        int temp=0;
-        //对角线交换
-        int n = matrix.length;
-        for (int i = 0; i < n; i++) {
-            for (int j =i+1; j < n; j++) {
-                //交换（i，j）和（j，i）
-                temp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int x = 0, y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            }
+            if (matrix[x][y] > target) {
+                --y;
+            } else {
+                ++x;
             }
         }
-        //逐行逆序
-        for (int i = 0; i < n; i++) {
-            for (int j =0; j < n/2; j++) {
-                temp=matrix[i][j];
-                matrix[i][j]=matrix[i][n-j-1];
-                matrix[i][n-j-1]=temp;
-            }
-        }
-
+        return false;
     }
 }
-
