31c82123b1902e9a8b9fa6157df7e7c91035ca86
Bun <2910752440@qq.com>
2024-03-08 18:24:13 +0800

    19. 删除链表的倒数第 N 个结点

--- LeetCode.java
+++ LeetCode.java
@@ -21,34 +21,28 @@ class ListNode {
 }
 
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1==null || l2==null)return l1==null?l2:l1;
-        ListNode head=new ListNode(l1.val+ l2.val);
-        ListNode res=head;
-
-        l1=l1.next;
-       l2=l2.next;
-        boolean temp=head.val>=10;
-        if (temp)head.val-=10;
-        while (l1!=null || l2!=null)
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode ago=head;
+        ListNode after=head;
+        ListNode pre=null;
+        int count=0;
+        while (ago!=null && count<n)
         {
-            int n1 = l1 != null ? l1.val : 0;
-            int n2 = l2 != null ? l2.val : 0;
-            int var=n1+n2;
-            if (temp) {
-                var ++;
-                temp=false;
-            }
-            if (var>=10) {
-                temp = true;
-                var-=10;
-            }
-            head.next=new ListNode(var);
-            head=head.next;
-            if (l1!=null)l1=l1.next;
-            if (l2!=null)l2=l2.next;
+            ago=ago.next;
+            count++;
         }
-        if (temp)head.next=new ListNode(1);
-        return res;
+        if (count!=n)return null;
+        while (ago!=null)
+        {
+            pre=after;
+            ago=ago.next;
+            after= after.next;
+        }
+        if (pre==null)return head.next;
+        pre.next=after.next;
+        after.next=null;
+
+        return head;
+
     }
 }
