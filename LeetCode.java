9251078e1e11095f0b63023f1ecb0d95094b2671
Bun <2910752440@qq.com>
2024-03-08 16:49:36 +0800

    142. 环形链表 II

--- LeetCode.java
+++ LeetCode.java
@@ -21,42 +21,25 @@ class ListNode {
 }
 
 class Solution {
+    public ListNode detectCycle(ListNode head) {
 
-    public boolean isPalindrome(ListNode head) {
-        ListNode midNode = findMidNode(head);
-        ListNode last = reverseList(midNode);
-        while (last!=null&&head!=null)
-        {
-            if (head.val!=last.val)return false;
-            last=last.next;
-            head=head.next;
-        }
-        return true;
-    }
-    public ListNode findMidNode(ListNode head)
-    {
         ListNode fast=head;
         ListNode slow=head;
         while (fast!=null&&fast.next!=null)
         {
             fast=fast.next.next;
             slow=slow.next;
+            if (slow==fast)
+            {
+                while (head!=slow)
+                {
+                    head=head.next;
+                    slow=slow.next
+                }
+                return head;
+            }
         }
-        return slow;
-    }
-    public ListNode reverseList(ListNode head) {
-        if (head==null ||head.next==null)return head;
-        ListNode pre=null;
-        ListNode behind=head.next;
 
-        while (behind!=null)
-        {
-            head.next=pre;
-            pre=head;
-            head=behind;
-            behind=head.next;
-        }
-        head.next=pre;
-        return head;
+        return null;
     }
 }
