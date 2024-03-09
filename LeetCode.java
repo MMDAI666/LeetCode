4063e7b0c61d1e48cb156f3394e4a771f876cfdf
Bun <2910752440@qq.com>
2024-03-09 14:38:18 +0800

    94. 二叉树的中序遍历

--- LeetCode.java
+++ LeetCode.java
@@ -9,117 +9,44 @@ public class LeetCode
     public static void main(String[] args)
     {
 
-        LRUCache cache=new LRUCache(2);
-        cache.put(2,1);
-        cache.put(1,1);
-        cache.put(2,3);
-        cache.put(4,1);
-        System.out.println(cache.get(1));
-        System.out.println(cache.get(2));
     }
 }
 
 
-class LRUCache
+class TreeNode
 {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-    class DLinkedNode {
-        int key;
-        int value;
-        DLinkedNode prev;
-        DLinkedNode next;
-        public DLinkedNode() {}
-        public DLinkedNode(int _key, int _value) {key = _key; value = _value;}
-    }
-
-    private int capacity;
-    private int size;
-    private DLinkedNode head,tail;
-
-    private HashMap<Integer,DLinkedNode> map;
-
-    public LRUCache(int capacity)
+    TreeNode()
     {
-        this.capacity=capacity;
-        size=0;
-        map=new HashMap<>(capacity);
-
-        head=new DLinkedNode();
-        tail = new DLinkedNode();
-
-        head.next=tail;
-
-        tail.prev=head;
-
     }
 
-    public int get(int key)
+    TreeNode(int val)
     {
-        DLinkedNode node = map.get(key);
-        if (node==null)return  -1;
-        node.prev.next=node.next;
-        node.next.prev=node.prev;
-
-        node.prev=head;
-        node.next=head.next;
-
-        head.next.prev=node;
-        head.next=node;
-        return node.value;
-
+        this.val = val;
     }
 
-    public void put(int key, int value)
+    TreeNode(int val, TreeNode left, TreeNode right)
     {
-        if (map.containsKey(key))
-        {
-            DLinkedNode node = map.get(key);
-
-            node.prev.next=node.next;
-            node.next.prev=node.prev;
-
-            node.prev=head;
-            node.next=head.next;
-
-            head.next.prev=node;
-            head.next=node;
-
-            node.value=value;
-
-        }
-        else if (size>=capacity)
-        {
-
-            DLinkedNode node=tail.prev;
-
-            node.prev.next=node.next;
-            node.next.prev=node.prev;
-
-            node.prev=head;
-            node.next=head.next;
-
-            head.next.prev=node;
-            head.next=node;
-
-
-            map.remove(node.key);
-            node.key=key;
-            node.value=value;
-            map.put(key,node);
-        }else
-        {
-            DLinkedNode node=new DLinkedNode(key,value);
-            node.prev=head;
-            node.next=head.next;
-
-            head.next.prev=node;
-            head.next=node;
-
-            map.put(key,node);
-            size++;
-
-        }
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
-
 }
+
+class Solution
+{
+    public List<Integer> inorderTraversal(TreeNode root)
+    {
+        List<Integer> ans=new ArrayList<>();
+        if (root==null)return ans;
+        ans.addAll(inorderTraversal(root.left));
+        ans.add(root.val);
+        ans.addAll(inorderTraversal(root.right));
+        return ans;
+    }
+}
+
 
