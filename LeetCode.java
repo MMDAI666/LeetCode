9c33fe3c7d778fbe4874a60546739ff8b23f8996
Bun <2910752440@qq.com>
2024-03-09 15:54:57 +0800

    543. 二叉树的直径

--- LeetCode.java
+++ LeetCode.java
@@ -37,20 +37,18 @@ class TreeNode
 }
 
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        return check(root,root);
-
+    private int len;
+    public int diameterOfBinaryTree(TreeNode root) {
+        len=0;
+        maxDepth(root);
+        return len-1;
     }
-    private boolean check(TreeNode left,TreeNode right)
-    {
-        if (left == null && right == null) {
-            return true;
-        }
-        if (left == null || right == null) {
-            return false;
-        }
-
-        return (left.val==right.val)&&check(left.left,right.right)&&check(left.right,right.left);
+    public int maxDepth(TreeNode root) {
+        if (root==null)return 0;
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        len=Math.max(l+r+1,len);
+        return Math.max(r,l)+1;
     }
 }
 
