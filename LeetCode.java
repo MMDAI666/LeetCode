4f95b6b9d1fd36c133c6e785c83c7b164430c9d3
Bun <2910752440@qq.com>
2024-03-09 15:27:20 +0800

    101. 对称二叉树

--- LeetCode.java
+++ LeetCode.java
@@ -37,9 +37,20 @@ class TreeNode
 }
 
 class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root==null)return 0;
-        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    public boolean isSymmetric(TreeNode root) {
+        return check(root,root);
+
+    }
+    private boolean check(TreeNode left,TreeNode right)
+    {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+
+        return (left.val==right.val)&&check(left.left,right.right)&&check(left.right,right.left);
     }
 }
 
