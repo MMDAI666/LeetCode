6d858d7cc0d0c39fcada871e8dd919a2b76f0b60
Bun <2910752440@qq.com>
2024-03-09 12:44:00 +0800

    148. 排序链表

--- LeetCode.java
+++ LeetCode.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.Node;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -12,55 +14,78 @@ public class LeetCode {
 
 
 class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
-        val = x;
-        next = null;
-    }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
 
 class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode hair=new ListNode(0,head);
-        ListNode pre=hair;
-        ListNode tail=hair;
-
-        while (head!=null)
+    public ListNode sortList(ListNode head) {
+        if(head==null||head.next==null)return head;
+        int length=0;
+        ListNode node=head;
+        while (node!=null)
         {
-            for (int i = 0; i < k; i++) {
-                tail=tail.next;
-                if (tail==null)return hair.next;
-            }
-
-            ListNode nex=tail.next;
-            ListNode[] re=myreverse(head,tail);
-            head=re[0];
-            tail=re[1];
-            tail.next=nex;
-            pre.next=head;
-
-            for (int i = 0; i < k; i++) {
-                pre=pre.next;
-                head=head.next;
+            node=node.next;
+            length++;
+        }
+        ListNode pre=new ListNode(0,head);
+        for (int subLength=1;subLength<length;subLength=subLength*2)
+        {
+            ListNode prev=pre,curr=pre.next;
+            while (curr!=null)
+            {
+                ListNode head1=curr;
+                for (int i=1;i<subLength&&curr.next!=null;i++)
+                {
+                    curr=curr.next;
+                }
+                ListNode head2=curr.next;
+                curr.next=null;
+                curr = head2;
+                for (int i=1;i<subLength&&curr!=null&& curr.next != null;i++)
+                {
+                    curr=curr.next;
+                }
+                ListNode nex=null;
+                if (curr!=null)
+                {
+                    nex=curr.next;
+                    curr.next=null;
+                }
+                ListNode merge = merge(head1, head2);
+                prev.next=merge;
+                while (prev.next!=null)
+                {
+                    prev=prev.next;
+                }
+                curr=nex;
             }
         }
-        return hair.next;
-
+        return pre.next;
     }
 
-    private ListNode[] myreverse(ListNode head, ListNode tail) {
-        ListNode prev = tail.next;
-        ListNode p = head;
-        while (prev!=tail)
-        {
-            ListNode nex = p.next;
-            p.next = prev;
-            prev = p;
-            p = nex;
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
         }
-        return new ListNode[]{tail,head};
-
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
     }
+
 }
