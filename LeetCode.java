6732239b1565d9e2252121024c1fa146b576ac5d
Bun <2910752440@qq.com>
2024-03-10 10:23:53 +0800

    102. 二叉树的层序遍历

--- LeetCode.java
+++ LeetCode.java
@@ -37,18 +37,25 @@ class TreeNode
 }
 
 class Solution {
-    private int len;
-    public int diameterOfBinaryTree(TreeNode root) {
-        len=0;
-        maxDepth(root);
-        return len-1;
-    }
-    public int maxDepth(TreeNode root) {
-        if (root==null)return 0;
-        int l = maxDepth(root.left);
-        int r = maxDepth(root.right);
-        len=Math.max(l+r+1,len);
-        return Math.max(r,l)+1;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans=new ArrayList<>();
+        if (root==null)return ans;
+        Deque<TreeNode> dq=new ArrayDeque<>();
+        dq.offerFirst(root);
+        while (!dq.isEmpty())
+        {
+            int len=dq.size();
+            List<Integer> res=new ArrayList<>();
+            for (int i = 0; i < len; i++)
+            {
+                TreeNode temp=dq.pollFirst();
+                res.add(temp.val);
+                if (temp.left!=null)dq.offerLast(temp.left);
+                if (temp.right!=null)dq.offerLast(temp.right);
+            }
+            ans.add(res);
+        }
+        return ans;
     }
 }
 
