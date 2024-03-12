7b5707d6b4aaa7034eb2c91bd04e17c3889e6ab1
Bun <2910752440@qq.com>
2024-03-12 13:18:08 +0800

    200. 岛屿数量

--- LeetCode.java
+++ LeetCode.java
@@ -11,43 +11,38 @@ public class LeetCode {
 }
 
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
 
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
 
 class Solution {
-    int sum=Integer.MIN_VALUE;;
-    public int maxPathSum(TreeNode root) {
-        maxSum(root);
-        return sum;
+    public int numIslands(char[][] grid) {
+        int ans=0;
+        for (int i = 0; i < grid.length; i++)
+        {
+            for (int j = 0; j < grid[0].length; j++)
+            {
+                if (dfs(grid,i,j))ans++;
+            }
+        }
+        return ans;
     }
 
-    public int maxSum(TreeNode root)
+    public boolean dfs(char[][] grid,int r,int c)
     {
-        if (root==null)return 0;
-        int left=Math.max(maxSum(root.left),0);
-        int right=Math.max(maxSum(root.right),0);
-        int maxsum = root.val + left+right;
+        if (!inArea(grid,r,c))return false;
+        if (grid[r][c]!='1')return false;
 
-        sum=Math.max(sum,maxsum);
+        grid[r][c]='2';
 
-        return root.val + Math.max(left,right);
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+        return true;
+    }
+
+    private boolean inArea(char[][] grid,int r,int c)
+    {
+        return 0<=r&&r<grid.length&&0<=c&&c<grid[0].length;
     }
 }
-
 
