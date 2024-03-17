6c954fdcbc7434ee2b51c5dbbf4c8b9ea00d55d8
Bun <2910752440@qq.com>
2024-03-17 15:42:27 +0800

    35. 搜索插入位置

--- LeetCode.java
+++ LeetCode.java
@@ -14,25 +14,34 @@ public class LeetCode
 }
 
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int n=nums.length;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        int n=matrix[0].length;
         int left=0;
-        int right=n-1;
-        int ans=n;
+        int right=m-1;
+        int index=0;
         while (left<=right)
         {
             int mid=(left+right)/2;
-            if (nums[mid]==target)return mid;
-            if (target<nums[mid])
-            {
-                ans=mid;
-                right=mid-1;
-            }
+            if (matrix[mid][0]==target)return true;
+            if (target<matrix[mid][0]) right=mid-1;
             else
             {
-                left=mid+1;
+                index=mid;
+                left = mid + 1;
             }
         }
-        return ans;
+
+        left=0;
+        right=n-1;
+        while (left<=right)
+        {
+            int mid=(left+right)/2;
+            if (matrix[index][mid]==target)return true;
+            if (target<matrix[index][mid])right=mid-1;
+            else left=mid+1;
+        }
+        return false;
+
     }
 }
