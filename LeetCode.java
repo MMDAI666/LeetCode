835f1cd71bad5e4ca2e60781ad9422a5db3a7d0a
Bun <2910752440@qq.com>
2024-03-17 19:47:43 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置

--- LeetCode.java
+++ LeetCode.java
@@ -9,39 +9,39 @@ public class LeetCode
 {
     public static void main(String[] args) throws IOException
     {
-
+        int[] a=new int[]{1};
+        new Solution().searchRange(a,1);
     }
 }
 
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int m=matrix.length;
-        int n=matrix[0].length;
+    public int[] searchRange(int[] nums, int target) {
+        int n=nums.length;
+        int[] res=new int[]{0,n};
         int left=0;
-        int right=m-1;
-        int index=0;
+        int right=n-1;
         while (left<=right)
         {
             int mid=(left+right)/2;
-            if (matrix[mid][0]==target)return true;
-            if (target<matrix[mid][0]) right=mid-1;
-            else
+            if (target<=nums[mid])
             {
-                index=mid;
-                left = mid + 1;
-            }
+                res[0]=mid;
+                right=mid-1;
+            }else left = mid + 1;
         }
-
         left=0;
         right=n-1;
         while (left<=right)
         {
             int mid=(left+right)/2;
-            if (matrix[index][mid]==target)return true;
-            if (target<matrix[index][mid])right=mid-1;
-            else left=mid+1;
+            if (target<nums[mid])
+            {
+                res[1]=mid;
+                right=mid-1;
+            }else left = mid + 1;
         }
-        return false;
-
+        res[1]--;
+        if (res[0]<=res[1] && nums[res[0]]==target && nums[res[1]]==target) return res;
+        return new int[]{-1,-1};
     }
 }
