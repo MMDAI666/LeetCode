c3b621a2516f44a45a59980c896b819425999a35
Bun <2910752440@qq.com>
2024-03-18 15:26:44 +0800

    153. 寻找旋转排序数组中的最小值

--- LeetCode.java
+++ LeetCode.java
@@ -13,29 +13,26 @@ public class LeetCode
 }
 
 class Solution {
-    public int search(int[] nums, int target) {
+    public int findMin(int[] nums) {
         int n=nums.length;
         int left=0;
         int right=n-1;
+        int min=Integer.MAX_VALUE;
         while (left<=right)
         {
             int mid=(left+right)/2;
-            if (nums[mid]==target)return mid;
+            if(nums[mid]<min)min=nums[mid];
             if (nums[left]<=nums[mid])
             {
-                if (nums[left]<=target && target<nums[mid])
-                {
-                    right=mid-1;
-                }
-                else left=mid+1;
-            }else if (nums[mid]<=nums[right])
+                if (min>nums[left])min=nums[left];
+                left=mid+1;
+
+
+            }else
             {
-                if (nums[mid]<target && target<=nums[right])
-                {
-                    left=mid+1;
-                }else right=mid-1;
+                right=mid-1;
             }
         }
-        return -1;
+        return min;
     }
 }
