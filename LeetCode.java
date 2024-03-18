cd89ae02099037aadd59d262537641a60f90f1c0
Bun <2910752440@qq.com>
2024-03-18 15:02:14 +0800

    33. 搜索旋转排序数组

--- LeetCode.java
+++ LeetCode.java
@@ -9,39 +9,33 @@ public class LeetCode
 {
     public static void main(String[] args) throws IOException
     {
-        int[] a=new int[]{1};
-        new Solution().searchRange(a,1);
     }
 }
 
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
+    public int search(int[] nums, int target) {
         int n=nums.length;
-        int[] res=new int[]{0,n};
         int left=0;
         int right=n-1;
         while (left<=right)
         {
             int mid=(left+right)/2;
-            if (target<=nums[mid])
+            if (nums[mid]==target)return mid;
+            if (nums[left]<=nums[mid])
             {
-                res[0]=mid;
-                right=mid-1;
-            }else left = mid + 1;
-        }
-        left=0;
-        right=n-1;
-        while (left<=right)
-        {
-            int mid=(left+right)/2;
-            if (target<nums[mid])
+                if (nums[left]<=target && target<nums[mid])
+                {
+                    right=mid-1;
+                }
+                else left=mid+1;
+            }else if (nums[mid]<=nums[right])
             {
-                res[1]=mid;
-                right=mid-1;
-            }else left = mid + 1;
+                if (nums[mid]<target && target<=nums[right])
+                {
+                    left=mid+1;
+                }else right=mid-1;
+            }
         }
-        res[1]--;
-        if (res[0]<=res[1] && nums[res[0]]==target && nums[res[1]]==target) return res;
-        return new int[]{-1,-1};
+        return -1;
     }
 }
