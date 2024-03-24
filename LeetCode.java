95fe015ef03c3397e9485e7de3dd045ee0b6a648
Bun <2910752440@qq.com>
2024-03-24 16:52:59 +0800

    739. 每日温度

--- LeetCode.java
+++ LeetCode.java
@@ -9,58 +9,31 @@ public class LeetCode
 {
     public static void main(String[] args) throws IOException
     {
-        String s = "3[z]2[2[y]pq4[2[jk]e1[f]]]ef";
-        System.out.println(new Solution().decodeString(s));
+
+        System.out.println(new Solution());
     }
 }
 
 class Solution
 {
-    public String decodeString(String s)
+    public int[] dailyTemperatures(int[] temperatures)
     {
-        Deque<Integer> numberStack = new ArrayDeque<>();
-        Deque<String> stringStack = new ArrayDeque<>();
-
-        StringBuilder res = new StringBuilder();
-
-        StringBuilder numsb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++)
+        Deque<Integer> stack = new ArrayDeque<>();
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++)
         {
-            if (s.charAt(i) <= '9' && s.charAt(i) >= '0')
+            if (stack.isEmpty()) stack.offerLast(i);
+            else
             {
-                numsb.append(s.charAt(i));
-                continue;
-            }
-            if (s.charAt(i) == ']')
-            {
-                int k = Objects.requireNonNull(numberStack.pollLast());
-
-                StringBuilder sb = new StringBuilder();
-                while (!(Objects.equals(stringStack.peekLast(), "["))) sb.append(stringStack.pollLast());
-                stringStack.pollLast();
-
-                stringStack.offerLast(String.valueOf(sb).repeat(k));
-                continue;
-            }
-            if (s.charAt(i) == '[')
-            {
-                numberStack.offerLast(Integer.valueOf(numsb.toString()));
-                numsb.delete(0, numsb.length());
-            }
-            stringStack.offerLast(String.valueOf(s.charAt(i)));
-        }
-
-        if (!stringStack.isEmpty())
-        {
-            int count = stringStack.size();
-            StringBuilder stemp = new StringBuilder();
-            for (int i = 0; i < count; i++)
-            {
-                stemp.append(stringStack.pollFirst()).reverse();
-                res.append(stemp);
-                stemp.delete(0, stemp.length());
+                while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peekLast()])
+                {
+                    int k = stack.pollLast();
+                    ans[k] = i - k;
+                }
+                stack.offerLast(i);
             }
         }
-        return res.toString();
+        return ans;
     }
 }
