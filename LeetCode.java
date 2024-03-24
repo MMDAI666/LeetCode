8242ddce0a48d78cdc9809ad786f62970cc5b0d1
Bun <2910752440@qq.com>
2024-03-24 15:55:19 +0800

    394. 字符串解码

--- LeetCode.java
+++ LeetCode.java
@@ -9,39 +9,58 @@ public class LeetCode
 {
     public static void main(String[] args) throws IOException
     {
-        System.out.println(new Solution());
+        String s = "3[z]2[2[y]pq4[2[jk]e1[f]]]ef";
+        System.out.println(new Solution().decodeString(s));
     }
 }
 
-class MinStack {
-    Deque<Integer> stack;
-    Deque<Integer> minStack;
+class Solution
+{
+    public String decodeString(String s)
+    {
+        Deque<Integer> numberStack = new ArrayDeque<>();
+        Deque<String> stringStack = new ArrayDeque<>();
 
-    public MinStack() {
-        stack=new ArrayDeque<>();
-        minStack=new ArrayDeque<>();
-        minStack.push(Integer.MAX_VALUE);
-    }
+        StringBuilder res = new StringBuilder();
 
-    public void push(int val) {
-        stack.push(val);
-        int min=minStack.peek();
-        if(val<min)min=val;
-        minStack.push(min);
-    }
+        StringBuilder numsb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s.charAt(i) <= '9' && s.charAt(i) >= '0')
+            {
+                numsb.append(s.charAt(i));
+                continue;
+            }
+            if (s.charAt(i) == ']')
+            {
+                int k = Objects.requireNonNull(numberStack.pollLast());
 
-    public void pop() {
-        stack.pop();
-        minStack.pop();
+                StringBuilder sb = new StringBuilder();
+                while (!(Objects.equals(stringStack.peekLast(), "["))) sb.append(stringStack.pollLast());
+                stringStack.pollLast();
 
-    }
+                stringStack.offerLast(String.valueOf(sb).repeat(k));
+                continue;
+            }
+            if (s.charAt(i) == '[')
+            {
+                numberStack.offerLast(Integer.valueOf(numsb.toString()));
+                numsb.delete(0, numsb.length());
+            }
+            stringStack.offerLast(String.valueOf(s.charAt(i)));
+        }
 
-    public int top() {
-
-        return stack.peek();
-    }
-
-    public int getMin() {
-        return minStack.peek();
+        if (!stringStack.isEmpty())
+        {
+            int count = stringStack.size();
+            StringBuilder stemp = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                stemp.append(stringStack.pollFirst()).reverse();
+                res.append(stemp);
+                stemp.delete(0, stemp.length());
+            }
+        }
+        return res.toString();
     }
 }
