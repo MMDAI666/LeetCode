3132b9672e10714aa0592a3bbb405770a2aac493
Bun <2910752440@qq.com>
2024-03-25 13:07:43 +0800

    215. 数组中的第K个最大元素

--- LeetCode.java
+++ LeetCode.java
@@ -14,33 +14,19 @@ public class LeetCode
     }
 }
 
-class Solution
-{
-    public int largestRectangleArea(int[] heights)
-    {
-        int n = heights.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        Deque<Integer> stack = new ArrayDeque<>();
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>((x,y)->y-x);
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < nums.length; i++)
         {
-            while (!stack.isEmpty() && heights[stack.peekLast()] >= heights[i]) stack.pollLast();
-            left[i] = stack.isEmpty() ? -1 : stack.peekLast();
-            stack.offerLast(i);
+            pq.add(nums[i]);
         }
-
-        stack.clear();
-        for (int i = n - 1; i >= 0; i--)
+        for (int i = 0; i < k-1; i++)
         {
-            while (!stack.isEmpty() && heights[stack.peekLast()] >= heights[i]) stack.pollLast();
-            right[i] = stack.isEmpty() ? n : stack.peekLast();
-            stack.offerLast(i);
+            pq.remove();
         }
+        return pq.peek();
 
-        int maxArea = 0;
-        for (int i = 0; i < n; i++) maxArea = Math.max(maxArea, (right[i] - left[i] - 1) * heights[i]);
-
-        return maxArea;
     }
 }
