296a018a5b6856a2100ca9fb1d1cceec74c39055
Bun <2910752440@qq.com>
2024-03-26 13:46:18 +0800

    198. 打家劫舍

--- LeetCode.java
+++ LeetCode.java
@@ -16,16 +16,21 @@ public class LeetCode
 
 
 class Solution {
-    public int climbStairs(int n) {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n<2)return nums[0];
+        //int[] dp=new int[n];
 
-        int p=0;
-        int q=0;
-        int r=1;
-        for (int i = 0; i <= n; i++)
+        int p=nums[0];
+        int q=Math.max(nums[0],nums[1]);
+        int r=Math.max(p,q);
+
+        for (int i = 2; i < n; i++)
         {
+            r=Math.max(p+nums[i],q);
             p=q;
             q=r;
-            r=p+q;
+            //dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
         }
         return r;
     }
