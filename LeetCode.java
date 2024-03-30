321b0e95096273caed3407b7bc6dff0dc597af58
Bun <2910752440@qq.com>
2024-03-30 17:21:26 +0800

    300. 最长递增子序列

--- LeetCode.java
+++ LeetCode.java
@@ -15,26 +15,24 @@ public class LeetCode
 
 
 class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> set=new HashSet<>(wordDict);
-
-        boolean[] dp=new boolean[s.length()+1];
-
-        dp[0]=true;
-        for (int i = 0; i <= s.length(); i++)
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length==1)return 1;
+        int n=nums.length;
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        int m=1;
+        for (int i = 0; i < n; i++)
         {
+            dp[i]=1;
             for (int j = 0; j < i; j++)
             {
-                if (dp[j] && set.contains(s.substring(j,i)))
-                {
-                    dp[i]=true;
-                    break;
-                }
+                if (nums[j]<nums[i])dp[i]=Math.max(dp[i],dp[j]+1);
             }
+            m=Math.max(m,dp[i]);
         }
-        return dp[s.length()];
-    }
 
+        return m;
+    }
 }
 
 
