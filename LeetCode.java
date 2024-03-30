36bf29aef634dfff251533056f7892f2cecd8cec
Bun <2910752440@qq.com>
2024-03-30 17:09:34 +0800

    139. 单词拆分

--- LeetCode.java
+++ LeetCode.java
@@ -15,20 +15,26 @@ public class LeetCode
 
 
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
-        int[] dp=new int[amount+1];
-        Arrays.fill(dp,amount+1);
-        dp[0]=0;
-        for (int i = 1; i <= amount; i++)
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set=new HashSet<>(wordDict);
+
+        boolean[] dp=new boolean[s.length()+1];
+
+        dp[0]=true;
+        for (int i = 0; i <= s.length(); i++)
         {
-            for (int j = 0; j < coins.length; j++)
+            for (int j = 0; j < i; j++)
             {
-                if (coins[j]<=i) dp[i]=Math.min(dp[i],dp[i-coins[j]]+1);
+                if (dp[j] && set.contains(s.substring(j,i)))
+                {
+                    dp[i]=true;
+                    break;
+                }
             }
         }
-        return dp[amount]>amount?-1:dp[amount];
+        return dp[s.length()];
     }
+
 }
 
 
